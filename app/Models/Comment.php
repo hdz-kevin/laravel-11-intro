eee1d89e6526948f918d435bfde70fa6e437c464
hdz-kevin <kevinhdez592@gmail.com>
2025-06-20 12:31:58 -0600

    migrations, models and relationships

---
+++ app/Models/Comment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'content',
+    ];
+
+    /**
+     * Get the user that created this comment.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+}
